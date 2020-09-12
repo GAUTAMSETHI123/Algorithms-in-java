ac1ab22489e833b6d9d33ca25717a79070550ab4
gautam <gautam.sethi_cs19@gla.ac.in>
2020-09-13 02:07:03 +0530

    use of flag for time complexity.

--- src/algorithms/BubbleSort.java
+++ src/algorithms/BubbleSort.java
@@ -29,14 +29,18 @@ public class BubbleSort {
         }
         System.out.println();
         int temp;
+        //int flag =1;
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
-
+                    //flag = 0;
                 }
+           // if(flag==1){
+                //break;
+
             }
         }
 
