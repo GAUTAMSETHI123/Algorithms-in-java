abc2dfcd77fb43406c5deb28217c61d51e392a16
gautam <gautam.sethi_cs19@gla.ac.in>
2020-09-13 01:51:18 +0530

    final commit

--- src/algorithms/BubbleSort.java
+++ src/algorithms/BubbleSort.java
@@ -15,11 +15,36 @@ public class BubbleSort {
         System.out.println("enter the length of the array");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int [] arr = new int[n];
+        int[] arr = new int[n];
+        System.out.println("enter array elements");
+        for (int index = 0; index < n; index++) {
+            arr[index] = scanner.nextInt();
+        }
 
+        System.out.println("array is :");
 
+        for (int index = 0; index < n; index++) {
+            System.out.print(arr[index] + " ");
 
+        }
+        System.out.println();
+        int temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 
+                }
+            }
+        }
 
+        System.out.println("sorted array is: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print( arr[i] + " ");
+
+            }
+
+        }
     }
-}
